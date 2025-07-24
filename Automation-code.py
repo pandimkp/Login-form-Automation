a9cae06ba184670d5d30e27aa768538ddddb06ee
Pandi Durai M <72220251+pandimkp@users.noreply.github.com>
2025-07-24 09:30:00 +0530

    Create Automation-code.py

---
+++ Automation-code.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("Automation Selenium")
+search_box.send_keys(Keys.RETURN)
+
+assert "Automation" in driver.title
+driver.quit()
